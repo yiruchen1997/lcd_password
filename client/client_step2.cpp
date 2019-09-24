8468deb1c5c4a1091fef65955ed1244e8d55e228
yiruChen <y2133256@gmail.com>
2019-09-24 09:51:20 +0800

    udp_to_tcp

---
+++ client/client_step2.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <cmath>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <net/if.h>
+#define RTT 150
+#define MSS 1024
+#define threshold 65535
+#define BUFFER_SIZE 32768
+#define CLI_PORT 10260
+
+using namespace std;
+
+typedef struct{
+	short src_port;
+	short dest_port;
+	int seq_num;
+	int ack_num;
+	short head_len:4, 
+		  not_use:6, 
+		  urg:1, 
+		  ack:1, 
+		  psh:1, 
+		  rst:1, 
+		  syn:1, 
+		  fin:1;
+	short rwnd;
+	short checknum;
+	short urg_pointer;
+	int options;
+	char data[1024];
+}Package;
+
+int port;
+char IP[100];
+char buffer[BUFFER_SIZE];
+char pkt_data[1024];
+
+Package send_pkt, recv_pkt;
+
+int main(int argc, char *argv[]){
+	if (argc < 5) {
+		fprintf(stderr,"Usage : %s <datafile>\n",argv[0]);
+		exit(errno);
+	}
+	
+	strcpy(IP, argv[1]);
+	port = atoi(argv[2]);
+	srand(time(NULL));
+		
+	static struct sockaddr_in server, client;
+	int server_len = sizeof(struct sockaddr_in);
+	int sockfd;
+	
+	/* Create server structure */
+	if (inet_pton(AF_INET, argv[1], &server.sin_addr) == 0){
+		printf("inet_pton : server empty\n");
+		exit(errno);
+	}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	
+	/* Create a UDP socket */
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		printf("socket fail\n");
+		exit(errno);
+	}
+	
+	/* Initialize address. */
+	memset(&client, 0, sizeof(client));
+	client.sin_family = AF_INET;
+	client.sin_port = htons(CLI_PORT);
+	client.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if(bind(sockfd, (struct sockaddr *) &client, sizeof(client)) == -1){
+		printf("bind fail\n");
+		exit(errno);
+	}
+	
+	/* Wait for a connection */
+	printf("=====Start the three-way handshake=====\n");
+	
+	/* send SYN */
+	memset(&send_pkt, 0, sizeof(send_pkt));
+	send_pkt.src_port = CLI_PORT;
+	send_pkt.dest_port = port;
+	send_pkt.seq_num = (short)(rand() % 10000 + 1);
+	//printf("send_pkt.seq_num = %d\n\n", send_pkt.seq_num);
+	send_pkt.syn = 1;
+	
+	memset(&pkt_data, 0, sizeof(pkt_data));
+	memcpy(pkt_data, &send_pkt, 24);
+	if(sendto(sockfd, pkt_data, 24, 0, (struct sockaddr *)&server, server_len) == -1){
+		printf("sendto fail\n");
+		exit(errno);
+	}
+	printf("Send a packet(SYN) to %s : %hu\n", argv[1], port);
+	
+	/* Wait for ACK */
+	if(recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server, (socklen_t *)&server_len) == -1){
+		printf("recvfrom fail\n");
+		exit(errno);
+	}
+	printf("Received a packet(SYN/ACK) from %s : %hu\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+	
+	memcpy(&recv_pkt, buffer, sizeof(recv_pkt));
+	printf("\tReceive a packet (seq_num = %d, ack_num = %d)\n", recv_pkt.seq_num, recv_pkt.ack_num);
+	
+	if(recv_pkt.syn && recv_pkt.ack && (recv_pkt.ack_num == send_pkt.seq_num + 1)){
+		/* send ACK */
+		send_pkt.src_port = CLI_PORT;
+		send_pkt.dest_port = port;
+		send_pkt.syn = 0;
+		send_pkt.ack = 1;
+		send_pkt.ack_num = recv_pkt.seq_num + 1;
+		send_pkt.seq_num = recv_pkt.ack_num;
+						
+		memset(&pkt_data, 0, sizeof(pkt_data));						
+		memcpy(pkt_data, &send_pkt, 24);
+		if(sendto(sockfd, pkt_data, 24, 0, (struct sockaddr *)&server, server_len) == -1){
+			printf("sendto fail\n");
+			exit(errno);
+		}
+		printf("Send a packet(ACK) to %s : %hu\n", argv[1], port);
+	}
+	else{
+		printf("Not wanted SYN/ACK\n");
+		exit(errno);
+	}
+	printf("=====Complete the three-way handshake=====\n");
+	
+	/* Send File Name */
+	FILE* new_fp;
+	char file_buf[BUFFER_SIZE];
+	int loss = 0;
+	
+	/* Set Timeout */
+	struct timeval timeout = {0, 500000};
+	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+	
+	for(int i=4;i<argc;i++){	
+		strcpy(file_buf, argv[i]);
+		if(sendto(sockfd, file_buf, BUFFER_SIZE, 0, (struct sockaddr *)&server, server_len) == -1){
+			printf("sendto fail\n");
+			exit(errno);
+		}
+		
+		new_fp = fopen(file_buf, "w");
+		
+		/* Wait For File */
+		int data_size = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server, (socklen_t *)&server_len);
+		if(data_size == 0){
+			printf("recvfrom fail\n");
+			exit(errno);
+		}
+		printf("Received %s from %s : %hu\n", file_buf, inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+		
+		while(1){
+			if(!loss){
+				memcpy(&recv_pkt, buffer, sizeof(recv_pkt));
+				printf("\tReceive a packet (seq_num = %d, ack_num = %d)\n", recv_pkt.seq_num, recv_pkt.ack_num);
+				
+				fwrite(recv_pkt.data, sizeof(char), data_size - 24, new_fp);
+
+				/* Feedback */
+				send_pkt.src_port = CLI_PORT;
+				send_pkt.dest_port = port;
+				send_pkt.syn = 1;
+				send_pkt.ack = 1;
+				send_pkt.ack_num = recv_pkt.seq_num + data_size - 24;
+				send_pkt.seq_num = recv_pkt.ack_num;
+				
+				memset(&pkt_data, 0, sizeof(pkt_data));						
+				memcpy(pkt_data, &send_pkt, 24);
+				if(sendto(sockfd, pkt_data, 24, 0, (struct sockaddr *)&server, server_len) == -1){
+					printf("sendto fail\n");
+					exit(errno);
+				}
+			}
+			else{
+				/* Packet loss */
+				send_pkt.src_port = CLI_PORT;
+				send_pkt.dest_port = port;
+				send_pkt.syn = 0;
+				send_pkt.ack = 1;
+				send_pkt.ack_num = recv_pkt.seq_num;
+				send_pkt.seq_num = recv_pkt.ack_num;
+				
+				memset(&pkt_data, 0, sizeof(pkt_data));						
+				memcpy(pkt_data, &send_pkt, 24);
+				if(sendto(sockfd, pkt_data, 24, 0, (struct sockaddr *)&server, server_len) == -1){
+					printf("sendto fail\n");
+					exit(errno);
+				}
+				loss = 0;
+			}
+			
+			/* Get Anthoer Package */
+			data_size = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server, (socklen_t *)&server_len);
+			if(data_size <= 0){
+				printf("\t< Packet loss >\n");
+				loss = 1;
+			}
+			
+			if(!strcmp(buffer, "[server] File transfer complete")) break;
+		}
+		printf("\n***%s Transfer Success!***\n", file_buf);
+	}	
+	strcpy(pkt_data, "[client] Received All");
+	if(sendto(sockfd, pkt_data, sizeof(pkt_data), 0, (struct sockaddr *)&server, server_len) == -1){
+		printf("sendto fail\n");
+		exit(errno);
+	}
+	
+	fclose(new_fp);
+	close(sockfd);
+	
+	return 0;
+}
---
+++ server/server_step1.cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <cmath>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <net/if.h>
+#define RTT 150
+#define MSS 1024
+#define threshold 65535
+#define BUFFER_SIZE 32768
+#define PKT_PORT 10240
+
+using namespace std;
+
+typedef struct{
+	short src_port;
+	short dest_port;
+	int seq_num;
+	int ack_num;
+	short head_len:4, 
+		  not_use:6, 
+		  urg:1, 
+		  ack:1, 
+		  psh:1, 
+		  rst:1, 
+		  syn:1, 
+		  fin:1;
+	short rwnd;
+	short checknum;
+	short urg_pointer;
+	int options;
+	char data[1024];
+}Package;
+
+int port;
+char IP[100];
+char buffer[BUFFER_SIZE];
+char pkt_data[1024];
+int cwnd = 1;
+int rwnd = 10240;
+
+int temp = 0;
+Package send_pkt, recv_pkt;
+
+int main(int argc, char *argv[]){
+	if (argc != 2) {
+		fprintf(stderr,"Usage : %s <datafile>\n",argv[0]);
+		exit(errno);
+	}
+	
+	port = atoi(argv[1]);
+	char IP_name[100];
+    struct hostent *p;
+    gethostname(IP_name, sizeof(IP_name));
+    p = gethostbyname(IP_name);
+    char *temp; 
+    temp = inet_ntoa(*(struct in_addr*)(p->h_addr_list[0]));
+	strcpy(IP, temp);
+	srand(time(NULL));
+	
+	/* Program start */
+	printf("====Parameter====\n");
+	printf("The RTT delay = %d ms\n", RTT);
+	printf("The threshold = %d bytes\n", threshold);
+	printf("The MSS = %d bytes\n", MSS);
+	printf("The buffer size = %d bytes\n", BUFFER_SIZE);
+	printf("Server's IP is %s\n", IP);
+	printf("Server is listening on port %d\n", port);
+	puts("===============");
+	
+	static struct sockaddr_in server, client;
+	int client_len = sizeof(struct sockaddr_in);
+	int sockfd;
+	
+	/* Create a UDP socket */
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		printf("socket fail\n");
+		exit(errno);
+	}
+	
+	/* Initialize address. */
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if(bind(sockfd, (struct sockaddr *) &server, sizeof(server)) == -1){
+		printf("bind fail\n");
+		exit(errno);
+	}
+	while(1){	
+		/* Wait for a connection */
+		printf("Listening for client...\n");
+		printf("=====Start the three-way handshake=====\n");
+		
+		/* Wait for SYN */
+		if(recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client, (socklen_t *)&client_len) == -1){
+			printf("recvfrom fail\n");
+			exit(errno);
+		}
+		printf("Received a packet(SYN) from %s : %hu\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+		
+		memcpy(&recv_pkt, buffer, sizeof(recv_pkt));
+		printf("\tReceive a packet (seq_num = %d, ack_num = %d)\n", recv_pkt.seq_num, recv_pkt.ack_num);
+		
+		int pid = fork();
+		if(pid == 0){
+			port = PKT_PORT;
+			
+			/* Create a UDP socket */
+			if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+				printf("socket fail\n");
+				exit(errno);
+			}
+			
+			/* Initialize address. */
+			memset(&server, 0, sizeof(server));
+			server.sin_family = AF_INET;
+			server.sin_port = htons(port);
+			server.sin_addr.s_addr = htonl(INADDR_ANY);
+			
+			bind(sockfd, (struct sockaddr *) &server, sizeof(server));
+			
+			if(recv_pkt.syn){
+				/* send SYN/ACK */
+				send_pkt.dest_port = recv_pkt.src_port;
+				send_pkt.src_port = port;
+				send_pkt.syn = 1;
+				send_pkt.ack = 1;
+				send_pkt.ack_num = recv_pkt.seq_num + 1;
+				send_pkt.seq_num = (short)(rand() % 10000 + 1);
+				
+				memset(&pkt_data, 0, sizeof(pkt_data));
+				memcpy(pkt_data, &send_pkt, 24);
+				if(sendto(sockfd, pkt_data, 24, 0, (struct sockaddr *)&client, client_len) == -1){
+					printf("sendto fail\n");
+					exit(errno);
+				}
+				printf("Send a packet(SYN/ACK) to %s : %hu\n", inet_ntoa(client.sin_addr), recv_pkt.src_port);
+			}
+			else{
+				printf("send_pkt SYN bit error\n");
+				exit(errno);
+			}
+			
+			/* Wait for ACK */
+			if(recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client, (socklen_t *)&client_len) == -1){
+				printf("recvfrom fail\n");
+				exit(errno);
+			}
+			printf("Received a packet(ACK) from %s : %hu\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+			
+			memcpy(&recv_pkt, buffer, sizeof(recv_pkt));
+			printf("\tReceive a packet (seq_num = %d, ack_num = %d)\n", recv_pkt.seq_num, recv_pkt.ack_num);
+			
+			printf("=====Complete the three-way handshake=====\n");
+			
+			/* Start Sending Files */
+			FILE* fp;
+			
+			while(1){
+				/* Get File Name */
+				if(recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client, (socklen_t *)&client_len) == -1){
+					printf("recvfrom fail\n");
+					exit(errno);
+				}
+				
+				if(!strcmp(buffer, "[client] Received All")) break;
+				
+				char file_buf[1024];
+				strcpy(file_buf, buffer);
+				//printf("file_buf = %s\n", file_buf);
+				
+				fp = fopen(file_buf, "r");
+				if(!fp){
+					printf("File open fail\n");
+					exit(errno);
+				}
+				
+				fseek(fp, 0, SEEK_END);
+				int file_size = ftell(fp);
+				fseek(fp, 0, SEEK_SET);
+				
+				printf("Start to send %s to %s : %hu, the file size is %d bytes.\n", file_buf, inet_ntoa(client.sin_addr), recv_pkt.src_port, file_size);
+				
+				int file_num = 1;
+				int read_byte = 1;
+				int data_size;
+				int print_msg = 1, tmp = 0, count = 0;
+				
+				while(1){
+					data_size = fread(send_pkt.data, sizeof(char), cwnd, fp);
+					if(data_size == 0) break;
+				
+					/* Send File */
+					send_pkt.dest_port = recv_pkt.src_port;
+					send_pkt.src_port = port;
+					send_pkt.syn = 1;
+					send_pkt.ack = 1;
+					send_pkt.ack_num = recv_pkt.seq_num + 1;
+					send_pkt.seq_num = file_num;
+					
+					if(cwnd < 1024) printf("cwnd = %d, rwnd = %d, threshold = %d\n", cwnd, rwnd, threshold);
+					else{
+						if(print_msg) printf("cwnd = %d, rwnd = %d, threshold = %d\n", (int)pow(2, 10+tmp), rwnd, threshold);
+						print_msg = 0;
+						count++;
+						
+						if(count >= pow(2, tmp)){
+							tmp++;
+							print_msg = 1;
+							count = 0;
+						}
+					}
+					
+					memcpy(pkt_data, &send_pkt, data_size + 24);
+					if(sendto(sockfd, pkt_data, data_size + 24, 0, (struct sockaddr *)&client, client_len) == -1){
+						printf("sendto fail\n");
+						exit(errno);
+					}
+					
+					if(cwnd < 1024) printf("\tSend a packet at : %d bytes\n", read_byte);
+					else printf("\tSend a packet at : 1024 bytes\n");
+					
+					bzero(&pkt_data, sizeof(pkt_data));
+					
+					/* Wait for Client Feedback */
+					if(recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client, (socklen_t *)&client_len) == -1){
+						printf("recvfrom fail\n");
+						exit(errno);
+					}
+					
+					memcpy(&recv_pkt, buffer, sizeof(recv_pkt));
+					printf("\tReceive a packet (seq_num = %d, ack_num = %d)\n", recv_pkt.seq_num, recv_pkt.ack_num);
+					
+					/* Check recv_pkt info */
+					if(recv_pkt.ack && recv_pkt.ack_num == read_byte + data_size){ 
+						file_num += cwnd;
+						if(cwnd < 1024) cwnd = cwnd * 2;
+						read_byte += data_size;
+					}else
+						fseek(fp, read_byte, SEEK_SET);
+				}	
+				strcpy(pkt_data, "[server] File transfer complete");
+				if(sendto(sockfd, pkt_data, sizeof(pkt_data), 0, (struct sockaddr *)&client, client_len) == -1){
+					printf("sendto fail\n");
+					exit(errno);
+				}
+				puts(""); cwnd = 1;
+			}
+			fclose(fp);
+			return 0;
+		}
+	}
+	close(sockfd);
+	
+	return 0;
+}
